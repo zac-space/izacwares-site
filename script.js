74f2500da72740c12f32b607e2ec1bd3232c5e32
zac <41367576+zac-space@users.noreply.github.com>
2025-12-14 20:53:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ window.addEventListener("scroll", () => {
     if (window.scrollY > 30) {
         header.classList.add("active");
         heroLogo.classList.add("hide");
+        document.body.classList.add("scrolled");
     } else {
         header.classList.remove("active");
         heroLogo.classList.remove("hide");
+        document.body.classList.remove("scrolled");
     }
 });
