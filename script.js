ca7004e077dc7e9f2c55b661df8621e9e49f4766
zac <41367576+zac-space@users.noreply.github.com>
2025-12-15 02:33:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const header = document.getElementById("main-header");
-const heroAssemble = document.getElementById("hero-assemble");
+const heroAssemble = document.querySelector(".logo-assemble");
 const wDark = document.querySelector(".w-dark");
 const wLight = document.querySelector(".w-light");
 
@@ -9,7 +9,7 @@ window.addEventListener("scroll", () => {
         document.body.classList.add("scrolled");
         // 헤더 표시
         header.classList.add("active");
-        // 중앙 로고 제거
+        // W 없는 로고 페이드 아웃
         heroAssemble.style.opacity = "0";
         // W 색상 전환
         wDark.style.opacity = "0";
