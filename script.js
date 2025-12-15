08fd9437215dc87ae441fddc3f7b7686856176c7
zac <41367576+zac-space@users.noreply.github.com>
2025-12-15 15:02:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const header = document.getElementById("main-header");
 const heroAssemble = document.querySelector(".logo-assemble");
 const wDark = document.querySelector(".w-dark");
 const wLight = document.querySelector(".w-light");
+const heroLogo = document.getElementById("hero-logo");
+let wMoved = false;
 
 window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY;
     if (window.scrollY > 30) {
-        // 배경 전환
         document.body.classList.add("scrolled");
-        // 헤더 표시
         header.classList.add("active");
-        // W 없는 로고 페이드 아웃
         heroAssemble.style.opacity = "0";
-        // W 색상 전환
         wDark.style.opacity = "0";
         wLight.style.opacity = "1";
     } else {
@@ -20,5 +19,9 @@ window.addEventListener("scroll", () => {
         heroAssemble.style.opacity = "1";
         wDark.style.opacity = "1";
         wLight.style.opacity = "0";
+    }
+    if (scrollY > 120 && !wMoved) {
+        heroLogo.classList.add("w-moved");
+        wMoved = true;
     }
 });
