08f624f5ccf9c339bbfba0c48544de89a5d5e898
zac <41367576+zac-space@users.noreply.github.com>
2025-12-15 15:15:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,30 @@ const heroAssemble = document.querySelector(".logo-assemble");
 const wDark = document.querySelector(".w-dark");
 const wLight = document.querySelector(".w-light");
 const heroLogo = document.getElementById("hero-logo");
-let wMoved = false;
 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
+    // Step-1
     if (window.scrollY > 30) {
         document.body.classList.add("scrolled");
         header.classList.add("active");
+        
         heroAssemble.style.opacity = "0";
         wDark.style.opacity = "0";
         wLight.style.opacity = "1";
     } else {
         document.body.classList.remove("scrolled");
         header.classList.remove("active");
+        
         heroAssemble.style.opacity = "1";
         wDark.style.opacity = "1";
         wLight.style.opacity = "0";
-
-        wMoved = false;
-        heroLogo.classList.remove("w-moved");
     }
+    // Step-2
     if (scrollY > 120 && !wMoved) {
         heroLogo.classList.add("w-moved");
-        wMoved = true;
+    }
+    else{
+        heroLogo.classList.remove("w-moved");
     }
 });
